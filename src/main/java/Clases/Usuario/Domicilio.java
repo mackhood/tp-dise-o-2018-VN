6821ba6559346654b30b58ae52bb5376606f10e1
Derek F <derekfernandez182@gmail.com>
2018-07-13 15:36:41 -0300

    Agregado transformador y zona geografica, falta pulir la logica un poco mas todavia

--- src/main/java/Clases/Usuario/Domicilio.java
+++ src/main/java/Clases/Usuario/Domicilio.java
@@ -1,16 +1,22 @@
 package Clases.Usuario;
 
+import Clases.ZonaGeografica.ZonaGeografica;
+
 public class Domicilio {
 
     public String calle;
     public int altura;
     public int piso;
     public char departamento;
-
-    public Domicilio(String calle, int altura, int piso, char departamento) {
+    public ZonaGeografica zona;
+    
+    public Domicilio(ZonaGeografica zona, String calle, int altura, int piso, char departamento) {
         this.calle = calle;
         this.altura = altura;
         this.piso = piso;
         this.departamento = departamento;
+    	this.zona = zona;
     }
+    
+
 }
