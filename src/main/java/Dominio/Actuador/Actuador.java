6f0b1e5afeb7a063634a9533e13cd6d5fc5bc2f3
Unknown <germanjugo@hotmail.com>
2018-08-06 01:37:05 -0300

    Corrijo atributo en interfaz
    Fixes #13

--- src/main/java/Dominio/Actuador/Actuador.java
+++ src/main/java/Dominio/Actuador/Actuador.java
@@ -5,7 +5,7 @@ import Dominio.Dispositivo.DispositivoInteligente;
 public interface Actuador {
     //Cada uno de los distintos Actuadores va a tener su propia logica a implementar en ejecutar
     //El actuador sabe a que dispositivo tiene que afectar
-    DispositivoInteligente dispInteligente = null;
+//    DispositivoInteligente dispInteligente = null;
 
     void ejecutar();
 
