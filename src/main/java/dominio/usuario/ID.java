f62e1a125b4992e9e18c8fa6aa344791353cc958
fernandosierra9 <fernandosierra9@gmail.com>
2018-09-13 16:16:19 -0300

    agregado de json de transformadores

--- src/main/java/dominio/usuario/ID.java
+++ src/main/java/dominio/usuario/ID.java
@@ -4,20 +4,15 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name = "id")
 public class ID {
-	@Id
-	@GeneratedValue( strategy= GenerationType.AUTO)
-	protected Long id;
 
 	@Enumerated(EnumType.STRING)
 	TiposId tipoID;
-	@Column(length=150)
-
 	String numeroID;
 
 	public ID(TiposId tipoID, String numeroID) {
 		this.tipoID = tipoID;
 		this.numeroID = numeroID;
 	}
+	public ID(){}
 }
