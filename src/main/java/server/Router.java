8a1798929ed1e4e11c5609ff7c86ee96bab3be2f
Ariel Galvan <galvanariel97@gmail.com>
2018-10-23 19:12:31 -0300

    Empezando tp UIWeb

---
+++ src/main/java/server/Router.java
@@ -0,0 +1,31 @@
+package server;
+
+import controllers.*;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import utils.BooleanHelper;
+import utils.HandlebarsTemplateEngineBuilder;
+
+public class Router {
+
+	public static void configure() {
+		HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder
+				.create()
+				.withDefaultHelpers()
+				.withHelper("isTrue", BooleanHelper.isTrue)
+				.build();
+		
+		Spark.staticFiles.location("/public");
+		
+		//ProyectosController proyectosController = new ProyectosController();
+		
+		Spark.get("/", HomeController::home, engine);
+		Spark.get("/login", LoginController::show, engine);
+		Spark.post("/login", LoginController::login, engine);
+		//Spark.get("/proyectos", proyectosController::listar, engine);
+		//Spark.get("/proyectos/new", proyectosController::nuevo, engine);
+		//Spark.get("/proyectos/:id", proyectosController::mostrar, engine);
+		//Spark.post("/proyectos", proyectosController::crear);
+	}
+
+}
