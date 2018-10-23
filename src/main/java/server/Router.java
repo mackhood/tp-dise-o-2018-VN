f22ee82d749dde8ede9643c284dc6ce985aeaac3
Ariel Galvan <galvanariel97@gmail.com>
2018-10-23 20:13:59 -0300

    a

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -15,7 +15,7 @@ public class Router {
 				.withHelper("isTrue", BooleanHelper.isTrue)
 				.build();
 		
-		Spark.staticFiles.location("/public");
+		Spark.staticFiles.location("/");
 		
 		//ProyectosController proyectosController = new ProyectosController();
 		
