3ab5aa44d491ec3e504188f4a44607437cb37e72
Derek F <derekfernandez182@gmail.com>
2018-10-27 05:27:06 -0300

    corrijo test que se rompieron con los cambios

--- src/main/java/servicio/CargarTransformadores.java
+++ src/main/java/servicio/CargarTransformadores.java
@@ -2,6 +2,7 @@ package servicio;
 
 import dominio.dispositivo.DispositivoEstandar;
 import dominio.dispositivo.DispositivoInteligente;
+import dominio.dispositivo.Intervalo;
 import dominio.repositories.RepositorioTransformadores;
 import dominio.transformador.Transformador;
 import dominio.usuario.Cliente;
@@ -14,6 +15,8 @@ import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
 import javax.persistence.EntityManager;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,13 +40,19 @@ public class CargarTransformadores implements WithGlobalEntityManager, Transacti
         withTransaction(() -> {
             Domicilio domicilio = new Domicilio("av cordoba", 1234, 7, 'A');
             ID id = new ID(TiposId.DNI, "10125789");
+            List<Intervalo> intervalosDeUso = new ArrayList<>();
+            Intervalo i1 = new Intervalo(LocalDateTime.of(2018,10,24,15,00),LocalDateTime.of(2018,10,24,19,00));
+            Intervalo i2 = new Intervalo(LocalDateTime.of(2018,10,26,10,30),LocalDateTime.of(2018,10,26,11,30));
+            intervalosDeUso.add(i1);
+            intervalosDeUso.add(i2);
+            
             DispositivoEstandar dispEstandar = new DispositivoEstandar.DispositivoEstandarBuilder("reloj")
                     .consumoEstimadoPorHora(5.0).equipoConcreto("reloj").horasDeUso(4.0).build();
             List<DispositivoEstandar> dispositivosEstandares = new ArrayList<>();
             dispositivosEstandares.add(dispEstandar);
 
             DispositivoInteligente aireAcondicionado3500 = new DispositivoInteligente.DispositivoInteligenteBuilder(
-                    "aireAcondicionado").equipoConcreto("De 3500 frigorias").horasDeUso(5.0)
+                    "aireAcondicionado").equipoConcreto("De 3500 frigorias").intervalosDeUso(intervalosDeUso)
                     .consumoEstimadoPorHora((double) 1.613).build();
 
             List<DispositivoInteligente> dispositivosInteligentes = new ArrayList<>();
