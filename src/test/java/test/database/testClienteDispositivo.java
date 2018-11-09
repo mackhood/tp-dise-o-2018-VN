eb64fddf23fcf55ac46c6c918f4dc0a1fe6979c6
Derek F <derekfernandez182@gmail.com>
2018-11-09 04:13:46 -0300

    Update src/test/java/test/database/testClienteDispositivo.java

--- src/test/java/test/database/testClienteDispositivo.java
+++ src/test/java/test/database/testClienteDispositivo.java
@@ -1,11 +1,18 @@
 package test.database;
 
+import dominio.actuador.Actuador;
+import dominio.actuador.OrdenEncenderDI;
 import dominio.dispositivo.Dispositivo;
 import dominio.dispositivo.DispositivoEstandar;
 import dominio.dispositivo.DispositivoInteligente;
 import dominio.dispositivo.Intervalo;
 import dominio.manager.ClienteManager;
+import dominio.regla.Regla;
 import dominio.repositories.RepositorioDispositivo;
+import dominio.sensor.Condicion;
+import dominio.sensor.CondicionPorIgual;
+import dominio.sensor.CondicionPorMayor;
+import dominio.sensor.CondicionPorMenor;
 import dominio.usuario.*;
 import dominio.zonageografica.Ubicacion;
 import org.junit.After;
@@ -13,6 +20,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
@@ -47,36 +57,53 @@ public class testClienteDispositivo extends AbstractPersistenceTest implements W
         //mismoCliente.setNombre("nombreNuevo");
         Ubicacion nuevaUbicacion = new Ubicacion(2, 2);
         mismoCliente.setUbicacion(nuevaUbicacion);
-        entityManager().persist(unCliente);
+        entityManager().persist(mismoCliente);
         //Lo mismo con el assert
         assertEquals(mismoCliente.getApellido(),ClienteManager.getInstance().buscarClienteDeLaBDPorUsuario("nombreApellido").getApellido());
         assertEquals(mismoCliente.getUsuario(),ClienteManager.getInstance().buscarClienteDeLaBDPorUsuario("nombreApellido").getUsuario());
         assertEquals(mismoCliente.getContrasenia(),ClienteManager.getInstance().buscarClienteDeLaBDPorUsuario("nombreApellido").getContrasenia());
         assertEquals(nuevaUbicacion.getPosicionX(),ClienteManager.getInstance().buscarClienteDeLaBDPorUsuario("nombreApellido").getUbicacion().getPosicionX());
         assertEquals(nuevaUbicacion.getPosicionY(),ClienteManager.getInstance().buscarClienteDeLaBDPorUsuario("nombreApellido").getUbicacion().getPosicionY());
-        //assertEquals(nuevaUbicacion.getPosicionX(),entityManager().createQuery("from Cliente c where nombre = 'nombreNuevo' and apellido = 'apellido' and contrasenia = 'password'", Cliente.class).getSingleResult().getUbicacion() .getPosicionX());
+        //
+        assertEquals(unCliente.getApellido(),mismoCliente.getApellido());
+        assertEquals(unCliente.getUsuario(),mismoCliente.getUsuario());
+        assertEquals(unCliente.getContrasenia(),mismoCliente.getContrasenia());
+  
+        assertTrue(unCliente.getUbicacion().equals(mismoCliente.getUbicacion()));
     }
 
     @Test
     public void testTraerDispositivoDeLaBDModificarElNombreYGrabarloCASOPRUEBA2() {
+    	
+    	Intervalo i1 = new Intervalo(LocalDateTime.of(2018,10,12,19,30),LocalDateTime.of(2018,10,12,21,45));
+    	Intervalo i2 = new Intervalo(LocalDateTime.of(2018,10,15,11,50),LocalDateTime.of(2018,10,15,13,00));
+    	Intervalo i3 = new Intervalo(LocalDateTime.of(2018,11,9,04,20),LocalDateTime.of(2018,11,10,00,10));
+    	
+    	List<Intervalo> intervalosDeUso = new ArrayList<>();
+    	intervalosDeUso.add(i1);
+    	intervalosDeUso.add(i2);
+    	
+    	
+    	DispositivoInteligente di = new DispositivoInteligente.DispositivoInteligenteBuilder("di").consumoEstimadoPorHora((double)200).intervalosDeUso(intervalosDeUso).build();
+    	entityManager().persist(di);
 
-        LocalDateTime horaEncendido = LocalDateTime.of(2018, 6, 8, 15, 30, 30, 100);
-        LocalDateTime horaApagado = LocalDateTime.of(2018, 6, 8, 21, 25, 30, 100);
-        Intervalo intervalo = new Intervalo(horaEncendido,horaApagado);
-        List<Intervalo> intervalos = new ArrayList<>();
-        intervalos.add(intervalo);
+        DispositivoInteligente diRecuperado = (DispositivoInteligente) entityManager().createQuery("from dispositivointeligente where nombre = 'di'").getSingleResult();
+        long idQuery = (long) entityManager().createNativeQuery("select idDispositivo from dispositivointeligente where nombre = 'di'").getSingleResult();
+        
+        
+        assertTrue(diRecuperado.getIntervalosDeUso().stream().anyMatch(i-> i.equals(i1)));
+        assertTrue(diRecuperado.getIntervalosDeUso().stream().anyMatch(i-> i.equals(i2)));
+        assertFalse(diRecuperado.getIntervalosDeUso().stream().anyMatch(i-> i.equals(i3))); 
 
-        DispositivoInteligente di = entityManager().find(DispositivoInteligente.class, new Long(13));
-        di.agregarListaIntervalos(intervalos);
-
-        di.setNombre("nombreModificado");
-        entityManager().persist(di);
-        //FALTA MOSTRAR POR CONSOLA TODOS LOS INTERVALOS DONDE ESTUVO ENCENDIDO EL DISPOSITIVO
-        assertEquals("nombreModificado", entityManager().find(DispositivoInteligente.class, new Long(13)).getNombre());
+        diRecuperado.setNombre("nombreModificado");
+        entityManager().persist(diRecuperado);
+        
+        assertEquals("nombreModificado", ((DispositivoInteligente) entityManager().createQuery("from dispositivointeligente where idDispositivo = :id").setParameter("id",idQuery)).getNombre());
     }
+    
     @Test
     public void testTraerClientePorUsuario()
-    {
+    {	   	
         //VerificarUsuario verificarUsuario = new VerificarUsuario();
         assertEquals(true,VerificarUsuario.verificar("galvanariel","password"));
     }
@@ -93,7 +120,32 @@ public class testClienteDispositivo extends AbstractPersistenceTest implements W
         entityManager().persist(dispositivoInteligente);
         Assert.assertEquals(3,cliente.getDispositivosInteligentes().size());
     }
+    
+    @Test
+    public void casoDePrueba3() {
 
+    	List<Intervalo> intervalosDeUso = new ArrayList<>();
+    	DispositivoInteligente di = new DispositivoInteligente.DispositivoInteligenteBuilder("di").consumoEstimadoPorHora((double)200).intervalosDeUso(intervalosDeUso).build();
+    	OrdenEncenderDI actuador = new OrdenEncenderDI(di);
+    	List<Condicion> condiciones = new ArrayList<>();
+    	Regla reglaTest = new Regla(actuador,condiciones);
+    	CondicionPorMayor mayorA20 = new CondicionPorMayor(reglaTest,20,"Temperatura");
+    	condiciones.add(mayorA20);
+    	
+    	entityManager().persist(di);
+    	entityManager().persist(actuador);
+    	entityManager().persist(reglaTest);
+    	entityManager().persist(mayorA20);
+    	
+    	Regla reglaRecuperada = (Regla) entityManager().createQuery("from regla").getSingleResult();
+    	reglaRecuperada.chequearCondicionesYEjecutar();
+    	CondicionPorMenor menorA30 = new CondicionPorMenor(reglaRecuperada,15,"Temperatura");
+    	reglaRecuperada.agregarCondicion(menorA30);
+    	entityManager().persist(reglaRecuperada);
+    	Regla reglaRecuperadaV2 = (Regla) entityManager().createQuery("from regla").getSingleResult();
+    	assertTrue(reglaRecuperadaV2.getCondicionesACumplir().contains(menorA30));
+    }
+    
     //@After
     public void rollback() {
         entityManager().getTransaction().rollback();
