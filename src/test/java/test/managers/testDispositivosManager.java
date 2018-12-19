ed0e4c1dec51afa8c66f2650d4230d2eb89bbf69
Unknown <germanjugo@hotmail.com>
2018-12-19 02:55:14 -0300

    subo test para manager arreglar los de transformadores

---
+++ src/test/java/test/managers/testDispositivosManager.java
@@ -0,0 +1,51 @@
+package test.managers;
+
+import org.junit.Test;
+import persistence.DispositivosManager;
+import persistence.TransformadorManager;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class testDispositivosManager {
+
+
+
+
+
+
+
+@Test
+public void testGetInstance() {
+
+    DispositivosManager instance = DispositivosManager.getInstance();
+
+
+    assertEquals(instance.getClass(),DispositivosManager.class);
+
+}
+@Test
+    public void getDispositivoInteligenteDeLaBDPorID() {
+
+        DispositivosManager instance = DispositivosManager.getInstance();
+
+
+        assertEquals(instance.getDispositivoInteligenteDeLaBDPorID((long) 9),null);
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
